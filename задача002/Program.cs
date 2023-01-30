b3d7fac463f2bc29df275959a9f587206177dadd
Evgen Dirt <bankir032@mail.ru>
2023-01-30 06:40:04 +0300

    Решеные первые 8 задач

---
+++ задача002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает
+// два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите число b ");
+int b = Convert.ToInt32(Console.ReadLine()!);
+int max = a;
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+Console.WriteLine($"a = {a}; b = {b} -> max = {max}");
---
+++ задача008/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Clear();
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine()!);
+Console.Write($"{num} -> ");
+int count = 1;
+while (count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+        count++;
+    }
+    else count++;
+}
