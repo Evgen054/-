c0c6e9e9d05180991d5dcc99ed1a9b671b2cb61c
Evgen Dirt <bankir032@mail.ru>
2023-02-19 22:16:57 +0300

    Добавленно решение задачи 50

---
+++ задача047/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] GetArray(int row, int col, double minValue, double maxValue)
+{
+    double[,] array = new double[row, col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+            array[i,j]= Math.Round(array[i,j], 1);
+            
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество строк маcсива: ");
+int row = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите количество cтолбцов маcсива: ");
+int col = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(row, col, 0, 99);
+
+PrintArray(array);
---
+++ задача050/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+//  в двумерном массиве, и возвращает значение этого элемента или же указание, 
+//  что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReturnValueElement(int[,] array, int a, int b)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int lengthR = array.GetLength(0) - 1;
+        int lengthC = array.GetLength(1) - 1;
+        if (a > lengthR || b > lengthC || a <= 0 || b <= 0)
+        {
+            Console.Write($"{a + 1}{b + 1} -> Такого числа нет");
+            break;
+        }
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == a && j == b)
+            {
+                Console.Write($"[Элемент найден -> {array[i, j]}");
+                break;
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение элемента строки: ");
+int elementRow = int.Parse(Console.ReadLine()!) - 1;
+Console.Write("Введите значение элемента строки: ");
+int elementCol = int.Parse(Console.ReadLine()!) - 1;
+
+int[,] array = GetArray(row, col, -10, 10);
+
+PrintArray(array);
+
+ReturnValueElement(array, elementRow, elementCol);
+
