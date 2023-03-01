43c770687800f9c2967bbf0b8e91dd7d1f770354
Evgen Dirt <bankir032@mail.ru>
2023-03-01 21:16:17 +0300

    Добавленно решение задачи 64-68

---
+++ задача063/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int n = 9;
+int count = 5;
+
+void NaturNumber(int n, int count)
+{
+    int temp = n;
+    if (n < count)
+    {
+        return;
+    }
+    else
+    {
+        NaturNumber(n - 1, count);
+
+
+        Console.Write(n);
+        Console.Write(" ");
+
+    }
+
+}
+
+
+NaturNumber(n, count);
---
+++ задача066/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNM(int n, int m)
+{
+    int res = n;
+    if (n == m)
+        return 0;
+    else
+    {
+        n++;
+        res = n + SumNM(n, m);
+        return res;
+    }
+}
+
+void SumFromMToN(int n, int m)
+{
+    Console.Write(SumNM(n - 1, m));
+}
+
+
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+
+SumFromMToN(n, m);
